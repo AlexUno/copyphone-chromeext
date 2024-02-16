9eb463c2b03ea0d039e869d8bf577933880616f3
Alexandr96Belov <sasha51315121@mail.ru>
2024-02-16 19:32:29 +0300

    fit: add a search for items by tags and adding a copy button to them

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 const $btnScan = document.getElementById("btn-scan");
+const $btnScanAll = document.getElementById("btn-scan-all");
 
 function onResult(frames) {
   if (!frames || !frames.length) {
@@ -12,23 +13,32 @@ function execScript(tabId) {
   chrome.scripting.executeScript(
     {
       target: { tabId, allFrames: true },
-      files: ["js/phone.js"],
+      files: ["js/parse_phone.js", "js/parse_elem.js"],
     },
     onResult
   );
   window.close();
 }
 
-function currentTabHandler(e) {
-  chrome.tabs.query({ active: true }, function (tabs) {
-    const tab = tabs[0];
+function tabQueryHandler(active = true) {
+  if (active) {
+    chrome.tabs.query({ active }, function (tabs) {
+      const tab = tabs[0];
 
-    if (tab) {
-      execScript(tab.id);
-    } else {
-      alert("Нет активных табов");
-    }
-  });
+      if (tab) {
+        execScript(tab.id);
+      } else {
+        alert("Нет активных табов");
+      }
+    });
+  } else {
+    chrome.tabs.query({}, function (tabs) {
+      for (let i = 0; i < tabs.length; i++) {
+        execScript(tabs[i].id);
+      }
+    });
+  }
 }
 
-$btnScan.addEventListener("click", currentTabHandler);
+$btnScan.addEventListener("click", (e) => tabQueryHandler());
+$btnScanAll.addEventListener("click", (e) => tabQueryHandler(false));
